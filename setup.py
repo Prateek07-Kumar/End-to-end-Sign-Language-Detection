bb41f48211ff5cc1deda930003877011bc8e199f
Prateek Chaurasia <111336976+Prateek07-Kumar@users.noreply.github.com>
2023-10-22 22:47:12 +0530

    cicd

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'signLanguages',
+    name = 'Prateek Kumar',
     version= '0.0.0',
     author= 'iNeuron',
-    author_email= 'boktiar@ineuron.ai',
+    author_email= 'prateekchaurasia791@gmail.com',
     packages= find_packages(),
     install_requires = []
 
